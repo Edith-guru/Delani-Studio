c1654069c35f9325608412bfd9b8da1c8e8b15dc
Edith-guru <edithnesa@gmail.com>
2022-03-13 05:01:19 +0300

    updated what we do icons to toggle with decription

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// design
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".design-showing").toggle();
+      $(".design-hidden").toggle();
+    });
+  });
+
+
+// development
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".development-showing").toggle();
+      $(".development-hidden").toggle();
+    });
+  });
+
+
+// project management
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".project-showing").toggle();
+      $(".project-hidden").toggle();
+    });
+  });
