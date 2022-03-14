c6e0b13d3135aed020dbc135bcaea0efd2282934
Edith-guru <edithnesa@gmail.com>
2022-03-15 00:32:54 +0300

    Addded transition to what we do icons jquery

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
-// design
-$(document).ready(function() {
-    $("p").click(function() {
-      $(".design-showing").toggle();
-      $(".design-hidden").toggle();
-    });
-  });
+$(".clickable").click(function(){
+  $(".design-hidden").slideToggle();
+  $(".design-showing").slideToggle();
+});
 
+$(".clickable2").click(function(){
+  $(".development-hidden").slideToggle();
+  $(".development-showing").slideToggle();
+});
 
-// development
-$(document).ready(function() {
-    $("p").click(function() {
-      $(".development-showing").toggle();
-      $(".development-hidden").toggle();
-    });
-  });
-
-
-// project management
-$(document).ready(function() {
-    $("p").click(function() {
-      $(".project-showing").toggle();
-      $(".project-hidden").toggle();
-    });
-  });
+$(".clickable3").click(function(){
+  $(".management-hidden").slideToggle();
+  $(".management-showing").slideToggle();
+});
